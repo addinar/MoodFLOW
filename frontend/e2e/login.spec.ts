40b8a1ba08dc8e581c5f4f73c7760bc3a1d66c5a
addinar <raddina24@gmail.com>
2026-03-23 19:43:49 -0400

    implemented end to end test

---
+++ frontend/e2e/login.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+
+// -- END TO END TEST
+// Checks if when the user logs in, the application redirects to the dashboard
+
+test("user can log in and gets redirected to dashboard", async ({ page }) => {
+    await page.goto("http://localhost:3002/login"); // go to login page
+  
+    // fill inputs
+    await page.fill('input[type="email"]', "test@test.com");
+    await page.fill('input[type="password"]', "password123");
+
+    await page.click("text=Sign In"); // click login and trigger handleSubmit
+  
+    await expect(page).toHaveURL(/dashboard/); // check redirect success
+});
