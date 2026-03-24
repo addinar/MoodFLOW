3fcc0286ecf2dce62e58ec63085675c387b6f06f
addinar <raddina24@gmail.com>
2026-03-23 21:31:00 -0400

    fix: simulate successful authentication

--- frontend/e2e/login.spec.ts
+++ frontend/e2e/login.spec.ts
@@ -5,6 +5,17 @@ import { test, expect } from "@playwright/test";
 
 test("user can log in and gets redirected to dashboard", async ({ page }) => {
     await page.goto("http://localhost:3000/login"); // go to login page
+
+    // suppose login request is successfull
+    await page.route("**/identitytoolkit.googleapis.com/**", route => {
+        route.fulfill({
+            status: 200,
+            body: JSON.stringify({
+                idToken: "fake-token",
+                email: "test@test.com",
+            }),
+        });
+    });
   
     // fill inputs
     await page.fill('input[type="email"]', "test@test.com");
