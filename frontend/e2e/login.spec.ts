7d7484f8d60891a080bb36c471bca66194fad153
addinar <raddina24@gmail.com>
2026-03-23 21:44:35 -0400

    implemented connection with firebase in ci pipeline

--- frontend/e2e/login.spec.ts
+++ frontend/e2e/login.spec.ts
@@ -5,24 +5,6 @@ import { test, expect } from "@playwright/test";
 
 test("user can log in and gets redirected to dashboard", async ({ page }) => {
     await page.goto("http://localhost:3000/login"); // go to login page
-
-    // suppose login request is successfull
-    await page.route('**/identitytoolkit.googleapis.com/**', async route => {
-        await route.fulfill({
-            status: 200,
-            contentType: 'application/json',
-            body: JSON.stringify({
-                kind: "identitytoolkit#VerifyPasswordResponse",
-                localId: "test-user-id",
-                email: "test@test.com",
-                displayName: "",
-                idToken: "fake-token",
-                registered: true,
-                refreshToken: "fake-refresh",
-                expiresIn: "3600"
-            }),
-        });
-    });
   
     // fill inputs
     await page.fill('input[type="email"]', "test@test.com");
